994d9f71e60b4e4a30c3a8ec00635ff4a3fa9817
Felipe <naturalconfiguration@gmail.com>
2020-09-11 00:19:18 +0100

    backup

--- code/MyRewards/XDiReward.py
+++ code/MyRewards/XDiReward.py
@@ -197,41 +197,48 @@ class CDi242Reward(BaseReward):
     def initialize(self, env):
         # overflow threshold to act
         self.overflow_threshold = dt_float(0) # 0.9 #default 0.95
-        self.reward_min = dt_float(-100) #dt_float(-20.0*env.n_line - 10*env.n_gen - 5.0)
+        self.reward_min = dt_float(-120) #dt_float(-20.0*env.n_line - 10*env.n_gen - 5.0)
         self.reward_max = dt_float(0.0)
         #self.pf_threshold = dt_float(0.75)
         #self.pmaxmin_threshold = dt_float(0.9) #0.95
+        self.last_actual_dispatch = 0
 
     def __call__(self, action, env, has_error, is_done, is_illegal, is_ambiguous):
         #if has_error or is_illegal or is_ambiguous:
         #    return dt_float(-10)
         #else:
-        if is_illegal:
-            leg_cost = -10
-        else:
-            leg_cost = 0
+        # if is_illegal:
+        #     leg_cost = -10
+        # else:
+        #     leg_cost = 0
 
         # overflow lower than is_illegal
         rho = np.abs(env.backend.get_relative_flow())
         over_flow = np.maximum(rho - self.overflow_threshold, 0)
         #val = np.sum(over_flow**2)/env.n_line
-        ovf_cost = dt_float(-40*(50**max(over_flow)/50))
+        ovf_cost = dt_float(-50*(50**max(over_flow)/50))
 
         # active and reactive punishment
-        ac_disp = np.abs(env.get_obs().actual_dispatch / env.gen_pmax)
-        disp_cost = dt_float(-30*np.sum(ac_disp**2)/np.sum(env.gen_redispatchable))
+        actual_dispatch = env.get_obs().actual_dispatch
+        ac_disp = np.abs(actual_dispatch / env.gen_pmax)
+        disp_cost = dt_float(-30*(50**(np.sum(ac_disp)/np.sum(env.gen_redispatchable))/50))
 
-        # pmax limits
-        # prod_p, prod_q, _ = env.backend.generators_info()
-        # prod_p = prod_p[[True,True,False,False,False,True]]
-        # pmax = np.abs(env.gen_pmax)[[True,True,False,False,False,True]]
-        # pmin = np.abs(env.gen_pmin)[[True,True,False,False,False,True]]
-        # over = np.maximum(prod_p/pmax - self.pmaxmin_threshold, 0)
-        # over_redisp_cost = dt_float(-30*np.sum(over**2))
-        # under = np.maximum((pmin - prod_p)/pmax + 1 - self.pmaxmin_threshold, 0)        
-        # under_redisp_cost = dt_float(-30*np.sum(under**2))
+        disp_diff =np.sum(np.abs(self.last_actual_dispatch - actual_dispatch))
+        if disp_diff == 0:
+            diff_cost = -20
+        else:
+            diff_cost = 0
+        if is_illegal == 0:
+            ilegal_cost = -40
+        else:
+            ilegal_cost = 0
 
-        return leg_cost + ovf_cost + disp_cost #+ over_redisp_cost + under_redisp_cost
+        if is_done:
+            self.last_actual_dispatch = 0
+        else:
+            self.last_actual_dispatch = actual_dispatch
+
+        return ovf_cost + disp_cost + diff_cost + ilegal_cost #+ over_redisp_cost + under_redisp_cost
 
 class CDi24Reward(BaseReward):
 
